52e6a4f82b28e7f91c2818921f247ca8680ffda6
Stefanie Knoth <stefanie.knoth@gmail.com>
2017-03-10 19:24:28 +0100

    bla

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : 'your-secret-clientID-here', // your App ID
-        'clientSecret'    : 'your-client-secret-here', // your App Secret
+        'clientID'        : '1770965719839075', // your App ID
+        'clientSecret'    : '15f20fb39e19f96c772cdcd30a5470c4', // your App Secret
         'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
